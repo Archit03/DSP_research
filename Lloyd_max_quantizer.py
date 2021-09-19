538446fb49605b268ff9f025a77644b31275915c
Archit Sood <architsood99@hotmail.com>
2021-09-19 15:26:58 +0530

    Update Lloyd_max_quantizer.py

--- Lloyd_max_quantizer.py
+++ Lloyd_max_quantizer.py
@@ -1,4 +1,3 @@
-import matplot.pyplot as plt
 import numpy as np 
 import numba
 from numba import prange
@@ -6,7 +5,7 @@ import scipy.integrate as integrate
 
 
 def normal_dist(x, mean=0.0, vari=1.0):
-    return (1.0(np.sqrt(2.0*np.pi*vari)))*np.exp((-np.power((x-mean),2.0)),2.0))/(2.0*vari))
+    return (1.0(np.sqrt(2.0*np.pi*vari)))*np.exp((-np.power((x-mean),2.0)),2.0)/(2.0*vari)
 
 
 def laplace_dist(x, mean=0.0, vari=1.0):
@@ -32,18 +31,57 @@ def MSE_loss(x, x_hat):
     x = np.array(x)
     x_hat_q = np.array(x_hat_q)
     assert np.size(x) == np.size(x_hat_q)
-    MSE = np.sum(np.power(x-x_hat_q, 2))/np.size(x)
+    MSE = np.sum(np.power(x-x_hat_q,2))/np.size(x)
     return MSE
 
+
 class LloydMaxQuantizer(object):
+    @numba.autojit
+    @staticmethod
+    def start_repo(x, bit):
+        '''
+        Generate representation of each threshold using
+        Args:
+           x: Input signal for 
+           bit: amt of bit
+        return:
+            threshold
+            
+        
+        '''
+        assert isinstance(bit, int)
+        x = np.array(x)
+        num_repre = np.power(2, bit)
+        step = (np.max(x)-np.min(x))/num_repre
+        
+        middle_point = np.mean(x)
+        repre = np.array([])
+        for i in prange(int(num_repre/2)):
+            repre = np.append(repre, middle_point+(i+1)*step)
+            repre = np.insert(repre, 0, middle_point-(i+1)*step)
+        return repre 
     
     @staticmethod
-    def start_repre(x, bit):
-        """
-        Generate repretation of each threshold using
-        Args:
-            x:input signal 
-            bit: amt of bit
-        return:
-              threshold
-        """
+    def threshold(repre):
+        t_q = np.zeros(zp.size(repre)-1)
+        for i in prange(len(repre)-1):
+            t_q[i] = 0.5*(repre[i] + repre[i+1])
+        return t_q
+    
+    @numba.autojit
+    def quant(x, thres, repre):
+        '''
+        quantization operation
+        '''
+        thre = np.append(thre, np.inf)
+        thre = np.insert(thre, 0, -np.inf)
+        for i in prange(len(thre) -1):
+            if i == 0:
+                x_hat_q = np.where(np.logical_and(x > thre[i], x <= thre[i+1]), 
+                                   np.full(np.size(x_hat_q), repre[i]), x_hat_q)
+            else:
+                x_hat_q = np.where(np.logical_and(x > thre[i], x < thre[i+1]), 
+                                   np.full(np.size(x_hat_q), repre[i]), x_hat_q)
+        return x_hat_q
+        
+    
